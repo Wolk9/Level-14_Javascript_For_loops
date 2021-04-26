ba2e97de77947bc55e8f94043ec4544f3b55cb30
Wolk9 <66083478+Wolk9@users.noreply.github.com>
2021-04-26 13:30:59 +0200

    Even Odd Reporter done

---
+++ README.md
@@ -0,0 +1,3 @@
+# Level-14_Javascript-for-loops
+
+For all information on my steps, see comments in my .js
---
+++ even-odd-reporter.js
@@ -0,0 +1,6 @@
+// Even Odd reporter
+
+for (i = 0; i <= 20; i++) {
+  const result = i % 2 == 0 ? "even" : "odd";
+  console.log("the number " + i + " is " + result);
+}
